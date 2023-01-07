128ec3a83d9917fa92b9697f0024b45567a2b65b
Scott Beamer <sbeamer@ucsc.edu>
2023-01-06 20:53:51 -0800

    bump scala and almond versions

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ pip3 install jupyter_nbextensions_configurator
 pip3 install RISE
 jupyter nbextension enable splitcell/splitcell
 curl -L -o coursier https://git.io/coursier-cli && chmod +x coursier
-SCALA_VERSION=2.13.4 ALMOND_VERSION=0.11.1
+SCALA_VERSION=2.13.10 ALMOND_VERSION=0.13.2
 ./coursier bootstrap -r jitpack \
     -i user -I user:sh.almond:scala-kernel-api_$SCALA_VERSION:$ALMOND_VERSION \
     sh.almond:scala-kernel_$SCALA_VERSION:$ALMOND_VERSION \
